36bbcb1490789708b3b3a5303971199f217c4de3
Sebas-2702 <josesta2702@gmail.com>
2026-08-07 23:50:47 -0500

    deber4: implementa CRUD de libros electronicos

--- deber4-docente_dao/dao/libro_electronico_dao.go
+++ deber4-docente_dao/dao/libro_electronico_dao.go
@@ -2,6 +2,7 @@ package dao
 
 import (
 	"database/sql"
+	"deber4-docente_dao/model"
 )
 
 type LibroElectronicoDAO struct {
@@ -28,5 +29,126 @@ func (dao *LibroElectronicoDAO) CreateTable() error {
 	`
 
 	_, err := dao.DB.Exec(query)
+	return err
+}
+
+func (dao *LibroElectronicoDAO) Create(libro model.LibroElectronico) (int64, error) {
+	query := `
+	INSERT INTO libros_electronicos
+	(titulo, autor, genero, isbn, formato, precio)
+	VALUES (?, ?, ?, ?, ?, ?);
+	`
+
+	result, err := dao.DB.Exec(
+		query,
+		libro.Titulo,
+		libro.Autor,
+		libro.Genero,
+		libro.ISBN,
+		libro.Formato,
+		libro.Precio,
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	return result.LastInsertId()
+}
+
+func (dao *LibroElectronicoDAO) GetByID(id int64) (*model.LibroElectronico, error) {
+	query := `
+	SELECT id, titulo, autor, genero, isbn, formato, precio
+	FROM libros_electronicos
+	WHERE id = ?;
+	`
+
+	libro := &model.LibroElectronico{}
+
+	err := dao.DB.QueryRow(query, id).Scan(
+		&libro.ID,
+		&libro.Titulo,
+		&libro.Autor,
+		&libro.Genero,
+		&libro.ISBN,
+		&libro.Formato,
+		&libro.Precio,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return libro, nil
+}
+
+func (dao *LibroElectronicoDAO) GetAll() ([]model.LibroElectronico, error) {
+	query := `
+	SELECT id, titulo, autor, genero, isbn, formato, precio
+	FROM libros_electronicos
+	ORDER BY id;
+	`
+
+	rows, err := dao.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var libros []model.LibroElectronico
+
+	for rows.Next() {
+		var libro model.LibroElectronico
+
+		err := rows.Scan(
+			&libro.ID,
+			&libro.Titulo,
+			&libro.Autor,
+			&libro.Genero,
+			&libro.ISBN,
+			&libro.Formato,
+			&libro.Precio,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		libros = append(libros, libro)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return libros, nil
+}
+
+func (dao *LibroElectronicoDAO) Update(libro model.LibroElectronico) error {
+	query := `
+	UPDATE libros_electronicos
+	SET titulo = ?, autor = ?, genero = ?, isbn = ?, formato = ?, precio = ?
+	WHERE id = ?;
+	`
+
+	_, err := dao.DB.Exec(
+		query,
+		libro.Titulo,
+		libro.Autor,
+		libro.Genero,
+		libro.ISBN,
+		libro.Formato,
+		libro.Precio,
+		libro.ID,
+	)
+
+	return err
+}
+
+func (dao *LibroElectronicoDAO) Delete(id int64) error {
+	query := `
+	DELETE FROM libros_electronicos
+	WHERE id = ?;
+	`
+
+	_, err := dao.DB.Exec(query, id)
 	return err
 }
